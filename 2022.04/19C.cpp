8571a7360c725aa4e06de0d49d0cce5a0358bc32
박준성 <hell-o-world@naver.com>
2022-04-19 15:12:38 +0000

    Solve: BOJ23088 2021.04.19 오늘의 골드 스케쥴이 하나 끝날 때마다 시간을 잘 갱신해주어야 함. 이미 큐에 넣어진 스케쥴에 대해서는 T를 상수 취급할 수 있다.

---
+++ 2022.04/19C.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+#define int long long
+
+#define INF ((1LL<<31)-1)
+#define LINF ((1LL<<61)-1)
+#define MOD 1000000007
+#define MOD2 998244353
+
+#define TC() int CASE_N;cin>>CASE_N;for(int CASE_I=1;CASE_I<=CASE_N;CASE_I++)
+#define FIX(a) cout<<fixed;cout.precision(a);
+
+#define rep(i,j,k) for(int i=(j);i<(k);i++)
+#define repe(i,j,k) for(int i=(j);i<=(k);i++)
+#define per(i,j,k) for(int i=(k)-1;i>=(j);i--)
+#define pere(i,j,k) for(int i=(k);i>=(j);i--)
+#define each(v,n) for(auto n:v)
+
+#define yesif(x) cout<<((x)?"YES":"NO")<<'\n'
+#define impif(x) if(x){cout<<-1<<'\n';continue;}
+
+#define ff first
+#define ss second
+#define pb push_back
+#define bp pop_back
+#define mp make_pair
+#define mt make_tuple
+#define itos to_string
+#define endl '\n'
+
+#define BLK << ' ' <<
+#define NLE << '\n'
+
+#define sz(a) (int)((a).size())
+#define all(v) (v).begin(), (v).end()
+#define uniq(v) sort((v).begin(), (v).end()); (v).erase(unique((v).begin(),(v).end()),(v).end()) 
+#define mset(arr, n) memset(arr, n, sizeof(arr));
+
+#define BIT(a,b) (((a)>>(b))&1)
+#define dxdy(dx,dy) int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1}, dy[8] = {1, -1, 0, 0, 1, -1, 1, -1}
+
+#define fsi(f) freopen(f, "r", stdin) 
+#define fso(f) freopen(f, "w", stdout)
+
+#define RANDOM()         random_device rand;mt19937 seed(rand())
+// int-random-value    : uniform_int_distribution<int> gen(min,max);
+// double-random-value : uniform_real_distribution<double> gen(min,max);
+// how-to-get-value    : gen(seed)
+
+using namespace std;
+
+template <typename T1, typename T2> ostream& operator<<(ostream& out, pair<T1, T2>& p) {out << '{' << p.first << ", " << p.second << '}'; return out;}
+template <typename T> ostream& operator<<(ostream& out, vector<T>& v) {int len = v.size(); for (int i = 0 ; i < len ; i++) out << v[i] << (i+1 == len ? '\n' : ' '); return out;}
+template <typename T> ostream& operator<<(ostream& out, vector<vector<T>>& v) {int len = v.size(); for (int i = 0 ; i < len ; i++) out << v[i]; return out;}
+
+typedef pair<int,int> pii;
+typedef tuple<int,int,int> tiii;
+typedef tuple<int,int,int,int> tII;
+typedef vector<int> vi;
+typedef vector<pair<int,int>> vp;
+typedef vector<tuple<int,int,int>> vt;
+typedef vector<string> vs;
+typedef vector<vector<int>> wi;
+typedef vector<vector<pair<int,int>>> wp;
+typedef vector<vector<tuple<int,int,int>>> wt;
+
+int pow(int a, int b, int c) {if (!b) return 1; int rtn = pow(a, b/2, c); rtn = rtn * rtn % c; if (b%2) {rtn = rtn * a % c;} return rtn;}
+int safe(int x, int y, int Mx, int My, int mx = 0, int my = 0) {return mx <= x && x < Mx && my <= y && y < My;}
+int mivs(int a, int m) {int u = 0, v = 1, k = m; while (a != 0) {int t = k / a; k -= t * a; swap(a, k); u -= t * v; swap(u, v);} return (u+m)%m;}
+
+bool cmp(tiii a, tiii b) {
+    if (get<0>(a) != get<0>(b)) return get<0>(a) < get<0>(b);
+    if (get<1>(a) != get<1>(b)) return get<1>(a) > get<1>(b);
+    return get<2>(a) > get<2>(b);
+}
+
+signed main() {
+    cin.tie(NULL)->sync_with_stdio(false);
+    
+    int n;
+    cin >> n;
+
+    priority_queue <tiii, vt, decltype(&cmp)> pq(cmp);
+
+    vi t(n), p(n), q(n);
+    rep(i, 0, n) cin >> t[i] >> p[i] >> q[i];
+
+    pq.push({p[0]-t[0], q[0], 1});
+    
+    int T = t[0];
+    for (int i = 1 ; i < n ;) {
+        for ( ; i < n && t[i] <= T ; i++) pq.push({p[i]-t[i], q[i], i+1});
+        
+        auto [tmp, d, idx] = pq.top();
+        pq.pop();
+
+        cout << idx << ' ';
+        if (i < n) T = max(T + d, t[i]);
+    }
+
+    for (; !pq.empty() ; pq.pop()) cout << get<2>(pq.top()) << ' ';
+    cout << endl;
+}
