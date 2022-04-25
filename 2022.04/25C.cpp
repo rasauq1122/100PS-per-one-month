149250e39be8d090e02c16e0a44a4b4a62cb12a6
박준성 <hell-o-world@naver.com>
2022-04-25 14:15:47 +0000

    Solve: BOJ9029 2022.04.25 오늘의 골드 최적화할 수 있는 부분에서 안해주면 시간초과날 것 같은 DP.

---
+++ 2022.04/25C.cpp
@@ -0,0 +1,144 @@
+////////////////////////////////////////
+//   Today : Mon Apr 25 22:50:34 2022 //
+//  Author : rasauq1122               //
+// Problem : BOJ9029                  //
+////////////////////////////////////////
+
+#include <bits/stdc++.h>
+
+#define INF ((1LL<<31)-1)
+#define LINF ((1LL<<61)-1)
+#define MOD 1000000007
+#define MOD2 998244353
+
+#define TC() int CASE_N;cin>>CASE_N;for(int CASE_I=1;CASE_I<=CASE_N;CASE_I++)
+#define FIX(a) cout<<fixed;cout.precision(a);
+
+#define rep(i,j,k) for(int i=(j);i<(k);i++)
+#define repe(i,j,k) for(int i=(j);i<=(k);i++)
+#define per(i,j,k) for(int i=(k)-1;i>=(j);i--)
+#define pere(i,j,k) for(int i=(k);i>=(j);i--)
+#define each(v,n) for(auto n:v)
+
+#define yesif(x) cout<<((x)?"YES":"NO")<<'\n'
+#define impif(x) if(x){cout<<-1<<'\n';continue;}
+
+#define ff first
+#define ss second
+#define pb push_back
+#define bp pop_back
+#define mp make_pair
+#define mt make_tuple
+#define itos to_string
+#define endl '\n'
+
+#ifdef LOCAL
+    #define print(x) cout << x
+    #define BLS ' ' <<
+    #define BLK << ' ' <<
+    #define BLE << ' '
+    #define NLE << '\n'
+#else
+    #define print(x)
+    #define BLS
+    #define BLK
+    #define BLE
+    #define NLE
+#endif
+
+#define sz(a) (int)((a).size())
+#define all(v) (v).begin(), (v).end()
+#define uniq(v) (sort((v).begin(), (v).end()), (v).erase(unique((v).begin(),(v).end()),(v).end()))
+#define mset(arr, n) memset(arr, n, sizeof(arr));
+
+#define BIT(a,b) (((a)>>(b))&1)
+#define dxdy(dx,dy) int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1}, dy[8] = {1, -1, 0, 0, 1, -1, 1, -1}
+
+#define fsi(f) freopen(f, "r", stdin) 
+#define fso(f) freopen(f, "w", stdout)
+
+#define RANDOM()         random_device rand;mt19937 seed(rand())
+// int-random-value    : uniform_int_distribution<int> gen(min,max);
+// double-random-value : uniform_real_distribution<double> gen(min,max);
+// how-to-get-value    : gen(seed)
+
+using namespace std;
+
+template <typename T1, typename T2> ostream& operator<<(ostream& out, pair<T1, T2>& p) {out << '{' << p.first << ", " << p.second << '}'; return out;}
+template <typename T> ostream& operator<<(ostream& out, vector<T>& v) {int len = v.size(); for (int i = 0 ; i < len ; i++) out << v[i] << (i+1 == len ? '\n' : ' '); return out;}
+template <typename T> ostream& operator<<(ostream& out, vector<vector<T>>& v) {int len = v.size(); for (int i = 0 ; i < len ; i++) out << v[i]; return out;}
+template <typename T> vector<T> vec(int n) {return vector<T>(n);}
+template <typename T, typename... Types> auto vec(int n, Types... sz) {auto nvec = vec<T>(sz...); return vector<decltype(nvec)>(n, nvec);}
+
+typedef pair<int,int> pii;
+typedef tuple<int,int,int> tiii;
+typedef tuple<int,int,int,int> tII;
+typedef vector<int> vi;
+typedef vector<vi> wi;
+typedef vector<pii> vp;
+typedef vector<vp> wp;
+typedef vector<tiii> vt;
+typedef vector<vt> wt;
+typedef long long lli;
+typedef pair<lli,lli> pll;
+typedef tuple<lli,lli,lli> tlll;
+typedef tuple<lli,lli,lli,lli> tLL;
+typedef vector<lli> vl;
+typedef vector<vl> wl;
+typedef vector<pll> vpl;
+typedef vector<vpl> wpl;
+typedef vector<tlll> vtl;
+typedef vector<vtl> wtl;
+
+int pow(int a, int b, int c) {if (!b) return 1; int rtn = pow(a, b/2, c); rtn = rtn * rtn % c; if (b%2) {rtn = rtn * a % c;} return rtn;}
+int safe(int x, int y, int Mx, int My, int mx = 0, int my = 0) {return mx <= x && x < Mx && my <= y && y < My;}
+int mivs(int a, int m) {int u = 0, v = 1, k = m; while (a != 0) {int t = k / a; k -= t * a; swap(a, k); u -= t * v; swap(u, v);} return (u+m)%m;}
+
+auto dp = vec<int>(201, 201, 201);
+auto vis = vec<bool>(201, 201, 201);
+
+int foo(int x, int y, int z) {
+    int &ret = dp[x][y][z];
+    
+    if (vis[x][y][z]) return ret;
+    vis[x][y][z] = 1;
+    
+    if (x == y && y == z) return ret = 1;
+
+    int nin = 200*200*200;
+    vector<int*> v = {&x, &y, &z};
+    rep(i, 0, 3) {
+        rep(d, 1, *(v[i])) {
+            int nx = x - d*(i == 0);
+            int ny = y - d*(i == 1);
+            int nz = z - d*(i == 2);
+
+            int p = min({nx, ny, nz});
+            int q = max({nx, ny, nz});
+
+            int tmp = foo(p, nx+ny+nz-p-q, q);
+
+            if (i == 0) nx = d;
+            if (i == 1) ny = d;
+            if (i == 2) nz = d;
+
+            p = min({nx, ny, nz});
+            q = max({nx, ny, nz});
+
+            tmp += foo(p, nx+ny+nz-p-q, q);
+            nin = min(nin, tmp);
+        }
+    }
+
+    return ret = nin;
+}
+
+signed main() {
+    cin.tie(NULL)->sync_with_stdio(false);
+
+    TC() {
+        int a, b, c;
+        cin >> a >> b >> c;
+        cout << foo(a, b, c) << endl;
+    }
+}
